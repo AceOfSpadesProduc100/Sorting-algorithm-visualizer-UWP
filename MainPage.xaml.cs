06233012f1d390dbb07b62676ef0b8b15855e0fa
MitchellProductions <37678351+MitchellProductions@users.noreply.github.com>
2021-10-27 16:22:51 -0400

    audiograph

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,6 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Media;
+using Windows.Media.Audio;
+using Windows.Media.MediaProperties;
+using Windows.Media.Render;
 using Windows.UI;
 using Windows.UI.Input;
 using Windows.UI.Popups;
@@ -8,12 +15,22 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
 using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Algo
 {
+    // We are initializing a COM interface for use within the namespace
+    // This interface allows access to memory at the byte level which we need to populate audio data that is generated
+    [ComImport]
+    [Guid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    internal unsafe interface IMemoryBufferByteAccess
+    {
+        void GetBuffer(out byte* buffer, out uint capacity);
+    }
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
@@ -45,6 +62,13 @@ namespace Algo
 
         //how many array accesses were needed for the sort
         private long arrAccesses;
+
+        //Audio variables
+        private AudioGraph graph;
+        private AudioDeviceOutputNode deviceOutputNode;
+        private AudioFrameInputNode frameInputNode;
+        public double theta = 0;
+
         public MainPage()
         {
             InitializeComponent();
@@ -56,9 +80,15 @@ namespace Algo
             Array.Sort(arr);
         }
 
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            await CreateAudioGraph();
+        }
+
         private void ShuffleBtn_Click(object sender, RoutedEventArgs e)
         {
             ShuffleArray();
+            frameInputNode.Start();
         }
 
         private void ShuffleArray()
@@ -105,6 +135,56 @@ namespace Algo
                     case 4: //none
                         break;
                     case 5: //max heapified
+                        for (int i = arr.Length / 2 - 1; i >= 0; i--)
+                        {
+                            Heapify(i, arr.Length);
+                        }
+                        selectedArr = new int[] { arr.Length - 1 };
+
+                        AddHistorySnap();
+
+                        void Heapify(int i, int topI)
+                        {
+                            int maxI = i;
+                            int leftChildI = i * 2 + 1;
+                            int rightChildI = i * 2 + 2;
+
+                            comparisons++;
+                            if (leftChildI < topI)
+                            {
+                                arrAccesses += 2;
+                                comparisons++;
+                                if (arr[leftChildI] > arr[maxI])
+                                {
+                                    maxI = leftChildI;
+                                }
+                            }
+
+                            comparisons++;
+                            if (rightChildI < topI)
+                            {
+                                arrAccesses += 2;
+                                comparisons++;
+                                if (arr[rightChildI] > arr[maxI])
+                                {
+                                    maxI = rightChildI;
+                                }
+                            }
+
+                            comparisons++;
+                            if (maxI != i)
+                            {
+                                int oldI = arr[i];
+                                arr[i] = arr[maxI];
+                                arr[maxI] = oldI;
+
+                                arrAccesses += 4;
+
+                                selectedArr = new int[] { i };
+                                AddHistorySnap();
+                                Heapify(maxI, topI);
+                            }
+                        }
                         break;
                     default: //none, option 4
                         break;
@@ -115,6 +195,7 @@ namespace Algo
             isPremade = false;
 
         }
+
 
         private void Visualize_Click(object sender, RoutedEventArgs e)
         {
@@ -534,6 +615,98 @@ namespace Algo
                     AddHistorySnap();
                     Heapify(maxI, topI);
                 }
+            }
+        }
+        private unsafe AudioFrame GenerateAudioData(uint samples)
+        {
+            // Buffer size is (number of samples) * (size of each sample)
+            // We choose to generate single channel (mono) audio. For multi-channel, multiply by number of channels
+            uint bufferSize = samples * sizeof(float);
+            AudioFrame frame = new(bufferSize);
+
+            using (AudioBuffer buffer = frame.LockBuffer(AudioBufferAccessMode.Write))
+            using (IMemoryBufferReference reference = buffer.CreateReference())
+            {
+                float* dataInFloat;
+
+                // Get the buffer from the AudioFrame
+                ((IMemoryBufferByteAccess)reference).GetBuffer(out byte* dataInBytes, out uint capacityInBytes);
+
+                // Cast to float since the data we are generating is float
+                dataInFloat = (float*)dataInBytes;
+
+                float freq = 1000; // choosing to generate frequency of 1kHz
+                float amplitude = 0.3f;
+                int sampleRate = (int)graph.EncodingProperties.SampleRate;
+                double sampleIncrement = freq * (Math.PI * 2) / sampleRate;
+
+                // Generate a 1kHz sine wave and populate the values in the memory buffer
+                for (int i = 0; i < samples; i++)
+                {
+                    for (int j = 0; j < arr.Length; j++)
+                    {
+                        dataInFloat[i] = amplitude * arr[j];
+                    }
+                    //double sinValue = amplitude * Math.Sin(theta);
+                    
+                    theta += sampleIncrement;
+                }
+            }
+
+            return frame;
+        }
+
+        private async Task CreateAudioGraph()
+        {
+            // Create an AudioGraph with default settings
+            AudioGraphSettings settings = new(AudioRenderCategory.Media);
+            CreateAudioGraphResult result = await AudioGraph.CreateAsync(settings);
+
+            if (result.Status != AudioGraphCreationStatus.Success)
+            {
+                // Cannot create graph
+                return;
+            }
+
+            graph = result.Graph;
+
+            // Create a device output node
+            CreateAudioDeviceOutputNodeResult deviceOutputNodeResult = await graph.CreateDeviceOutputNodeAsync();
+            if (deviceOutputNodeResult.Status != AudioDeviceNodeCreationStatus.Success)
+            {
+                // Cannot create device output node
+            }
+
+            deviceOutputNode = deviceOutputNodeResult.DeviceOutputNode;
+
+            // Create the FrameInputNode at the same format as the graph, except explicitly set mono.
+            AudioEncodingProperties nodeEncodingProperties = graph.EncodingProperties;
+            nodeEncodingProperties.ChannelCount = 1;
+            frameInputNode = graph.CreateFrameInputNode(nodeEncodingProperties);
+            frameInputNode.AddOutgoingConnection(deviceOutputNode);
+
+            // Initialize the Frame Input Node in the stopped state
+            frameInputNode.Stop();
+
+            // Hook up an event handler so we can start generating samples when needed
+            // This event is triggered when the node is required to provide data
+            frameInputNode.QuantumStarted += Node_QuantumStarted;
+
+            // Start the graph since we will only start/stop the frame input node
+            graph.Start();
+        }
+
+        private void Node_QuantumStarted(AudioFrameInputNode sender, FrameInputNodeQuantumStartedEventArgs args)
+        {
+            // GenerateAudioData can provide PCM audio data by directly synthesizing it or reading from a file.
+            // Need to know how many samples are required. In this case, the node is running at the same rate as the rest of the graph
+            // For minimum latency, only provide the required amount of samples. Extra samples will introduce additional latency.
+            uint numSamplesNeeded = (uint)args.RequiredSamples;
+
+            if (numSamplesNeeded != 0)
+            {
+                AudioFrame audioData = GenerateAudioData(numSamplesNeeded);
+                frameInputNode.AddFrame(audioData);
             }
         }
     }
